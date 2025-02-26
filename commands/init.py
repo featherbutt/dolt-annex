0b182bf9f7e106e53d74d6d91502be33879c42ee
Tobias Featherbutt <iamjohndoe220@gmail.com>
2025-02-26 13:16:37 -0800

    add no_git and no_dolt flags to init

--- commands/init.py
+++ commands/init.py
@@ -11,42 +11,55 @@ class Init(cli.Application):
 
     parent: Application
 
+    no_dolt = cli.Flag(
+        "--no-dolt",
+        help = "Don't initialize the Dolt repository",
+    )
+
+    no_git = cli.Flag(
+        "--no-git",
+        help = "Don't initialize the Git repository",
+    )
+
     def main(self):
         config = self.parent.config
-        dolt = local.cmd.dolt.with_cwd(config.dolt_dir)
-        git = local.cmd.git["-C", config.git_dir]
 
-        git_config = git["config", "--local"]
-        git_annex = git["annex"]
+        if not self.no_dolt:
+            dolt = local.cmd.dolt.with_cwd(config.dolt_dir)
+            dolt("clone", config.dolt_remote, config.dolt_dir)
+            #dolt("init")
+            #dolt("remote", "add", "origin", config.dolt_remote)
+            #dolt("pull", "origin", "main")
 
-        dolt("clone", config.dolt_remote, config.dolt_dir)
-        #dolt("init")
-        #dolt("remote", "add", "origin", config.dolt_remote)
-        #dolt("pull", "origin", "main")
+        if not self.no_git:
+            git = local.cmd.git["-C", config.git_dir]
 
-        git("init", "--bare")
-        
-        git_config("user.name", config.name)
-        git_config("user.email", config.email)
-        git("-c", "annex.tune.objecthashlower=true", "annex", "init")
-        git_config("annex.commitmessage", config.annexcommitmessage)
-        if is_wsl():
-            git_config("annex.crippledfilesystem", "true")
-        #git("remote", "add", "origin", git_remote)
-        #git("fetch", "origin", "git-annex")
-        
-        git_annex("mincopies", "3")
-        git_annex("numcopies", "3")
+            git_config = git["config", "--local"]
+            git_annex = git["annex"]
 
-        git_annex("initremote", "--sameas=web", "tor", "type=web", "urlinclude=*//*.onion/*")
-        git_config("remote.tor.cost", "300")
+            git("init", "--bare")
+            
+            git_config("user.name", config.name)
+            git_config("user.email", config.email)
+            git("-c", "annex.tune.objecthashlower=true", "annex", "init")
+            git_config("annex.commitmessage", config.annexcommitmessage)
+            if is_wsl():
+                git_config("annex.crippledfilesystem", "true")
+            #git("remote", "add", "origin", git_remote)
+            #git("fetch", "origin", "git-annex")
+            
+            git_annex("mincopies", "3")
+            git_annex("numcopies", "3")
 
-        git_annex("initremote", "--sameas=web", "nontor", "type=web", "urlexclude=*//*.onion/*")
-        git_config("remote.nontor.cost", "100")
+            git_annex("initremote", "--sameas=web", "tor", "type=web", "urlinclude=*//*.onion/*")
+            git_config("remote.tor.cost", "300")
 
-        git_annex("enableremote", "tor")
-        git_annex("enableremote", "nontor")
+            git_annex("initremote", "--sameas=web", "nontor", "type=web", "urlexclude=*//*.onion/*")
+            git_config("remote.nontor.cost", "100")
 
-        git_annex("untrust", "web")
-        git_annex("untrust", "tor")
-        git_annex("untrust", "nontor")
+            git_annex("enableremote", "tor")
+            git_annex("enableremote", "nontor")
+
+            git_annex("untrust", "web")
+            git_annex("untrust", "tor")
+            git_annex("untrust", "nontor")
