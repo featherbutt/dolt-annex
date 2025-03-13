729d940adaa12d81475fe616f407e97370c6eea0
Tobias Featherbutt <iamjohndoe220@gmail.com>
2025-03-13 13:24:00 -0700

    update falr importer.

--- commands/import_command.py
+++ commands/import_command.py
@@ -90,7 +90,7 @@ class Import(cli.Application):
         elif self.from_md5:
             return importers.MD5Importer()
         elif self.from_falr:
-            return importers.FALRImporter(downloader.dolt_server, "gallery-archive", "fa")
+            return importers.FALRImporter(downloader.dolt_server, "filenames", "filenames")
         else:
             return None
 
