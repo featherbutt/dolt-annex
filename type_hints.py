04299dd13e4e0970f01071528aa0a682b0cc6461
Tobias Featherbutt <iamjohndoe220@gmail.com>
2025-03-22 16:07:43 -0700

    Add type hints

---
+++ type_hints.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Utility type declarations"""
+
+from typing import NewType
+
+
+PathLike = NewType('PathLike', str)
+AnnexKey = NewType('AnnexKey', str)
