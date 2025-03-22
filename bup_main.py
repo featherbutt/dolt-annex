3e4cb0ab5bc9b5ad5895f3ab99387777c7158902
Tobias Featherbutt <iamjohndoe220@gmail.com>
2025-03-21 18:26:43 -0700

    Minor formatting fix

--- bup_main.py
+++ bup_main.py
@@ -1,5 +1,7 @@
-# bup_main is a C module that provides the main entry point for the bup
-# command-line tool. It is implemented in bup_main.c.
+"""
+bup_main is a C module that provides the main entry point for the bup
+command-line tool. It is implemented in bup_main.c.
 
-# We don't depend on bup_main, but some of the bup modules try to import it.
-# We define an empty module here to prevent import errors.
+We don't depend on bup_main, but some of the bup modules try to import it.
+We define an empty module here to prevent import errors."
+"""
