619d9c8ebc3a63c89204d67006cf401bc1607afe
Tobias Featherbutt <iamjohndoe220@gmail.com>
2025-10-13 17:33:43 -0700

    Update init command
    The old version was incredibly brittle

--- dolt_annex/commands/init.py
+++ dolt_annex/commands/init.py
@@ -47,41 +47,16 @@ class Init(cli.Application):
             print("Unexpected positional arguments: ", args)
             self.help()
             return 1
-
-        # Things that need to be created:
-        # - dolt directory/repository (if it doesn't exist)
-        # - skip.sqlite3 database (if it doesn't exist)
-        # - uuid file (if it doesn't exist)
-        # - If a dolt-url is provided, add it as a remote to the dolt repository
-        # - If a dolt-url is provided, fetch from it and create a branch for this UUID?
         
-        if not self.no_dolt:
-            dolt_dir = self.parent.config.dolt_dir
-            if not dolt_dir.exists() or not (dolt_dir / ".dolt").exists():
-                print(f"Dolt directory {dolt_dir} does not exist. Creating it.")
-                dolt_dir.mkdir(parents=True, exist_ok=True)
-                shutil.copytree(data_dir / "dolt_base" / ".dolt", dolt_dir / ".dolt")
-                print(f"Initialized Dolt repository in {dolt_dir}")
-                # Add dolt remote if it was provided
-                # dolt = local.cmd.dolt.with_cwd(base_config.dolt_dir)
-                # dolt("config", "--local", "--add", "push.autoSetupRemote", "true")
+        init_config = InitConfig(
+            init_dolt = not self.no_dolt,
+            dolt_url = self.dolt_url,
+            remote_name = self.remote_name
+        )
+        do_init(self.parent.config, init_config)
+        return 0
 
-        local_uuid = read_uuid()
-        print(f"Local UUID: {local_uuid}")
-
-        if not Path("skip.sqlite3").exists():
-            shutil.copy(skip_db_path, "skip.sqlite3")
-
-        # TODO: Add .remote file?
-
-        # Create config file
-        if not Path("config.json").exists():
-
-            with open("config.json", "w", encoding="utf-8") as f:
-                path = Path("config.json")
-                with open(path, "w", encoding="utf-8") as f:
-                    json.dump(asdict(self.parent.config), f, ensure_ascii=False, indent=4)
-                print("Created config.json")
+        
 
 def read_uuid() -> uuid.UUID:
     try:
@@ -93,3 +68,42 @@ def read_uuid() -> uuid.UUID:
         with open("uuid", "w", encoding="utf-8") as fd:
             fd.write(str(local_uuid))
     return local_uuid
+
+def do_init(base_config, init_config: InitConfig):
+    # Things that need to be created:
+    # - dolt directory/repository (if it doesn't exist)
+    # - skip.sqlite3 database (if it doesn't exist)
+    # - uuid file (if it doesn't exist)
+    # - If a dolt-url is provided, add it as a remote to the dolt repository
+    # - If a dolt-url is provided, fetch from it and create a branch for this UUID?
+
+    if init_config.init_dolt:
+        dolt_dir = base_config.dolt_dir
+        if not dolt_dir.exists() or not (dolt_dir / ".dolt").exists():
+            print(f"Dolt directory {dolt_dir} does not exist. Creating it.")
+            dolt_dir.mkdir(parents=True, exist_ok=True)
+            shutil.copytree(data_dir / "dolt_base" / ".dolt", dolt_dir / ".dolt")
+            print(f"Initialized Dolt repository in {dolt_dir}")
+            # Add dolt remote if it was provided
+            dolt = local.cmd.dolt.with_cwd(base_config.dolt_dir)
+            dolt("config", "--local", "--add", "push.autoSetupRemote", "true")
+            if init_config.dolt_url:
+                dolt("remote", "add", init_config.remote_name, init_config.dolt_url)
+                dolt("fetch", init_config.remote_name)
+
+    local_uuid = read_uuid()
+    print(f"Local UUID: {local_uuid}")
+
+    if not Path("skip.sqlite3").exists():
+        shutil.copy(skip_db_path, "skip.sqlite3")
+
+    # TODO: Add .remote file?
+
+    # Create config file
+    if not Path("config.json").exists():
+
+        with open("config.json", "w", encoding="utf-8") as f:
+            path = Path("config.json")
+            with open(path, "w", encoding="utf-8") as f:
+                json.dump(asdict(base_config), f, ensure_ascii=False, indent=4)
+            print("Created config.json")
