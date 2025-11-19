4eb308635d2d7b9610a32402e138df1f07073769
Tobias Featherbutt <iamjohndoe220@gmail.com>
2025-11-19 12:14:06 -0800

    Add insert-record command, for use in tests

---
+++ src/dolt_annex/commands/insert_record.py
@@ -0,0 +1,83 @@
+from ast import arg
+import asyncio
+from typing import cast
+
+from plumbum import cli # type: ignore
+
+from dolt_annex.commands.import_command import move_files
+from dolt_annex.datatypes.common import TableRow
+from dolt_annex.datatypes.table import DatasetSchema
+from dolt_annex.application import Application
+from dolt_annex.file_keys import get_file_key_type
+from dolt_annex.table import Dataset
+
+class InsertRecord(cli.Application):
+    """Insert a single record into the annex and database. Primarily used for testing."""
+
+    parent: Application
+
+    dataset = cli.SwitchAttr(
+        "--dataset",
+        str,
+        help="The name of the dataset being imported to",
+        mandatory = True
+    )
+
+    table_name = cli.SwitchAttr(
+        "--table-name",
+        str,
+        help="The name of the table being imported to",
+        mandatory = True
+    )
+
+    key_columns = cli.SwitchAttr(
+        "--key-columns",
+        str,
+        help="The name of the dataset being imported to",
+        mandatory = True
+    )
+
+    file_bytes = cli.SwitchAttr(
+        "--file-bytes",
+        str,
+        help="The bytes of the file to insert",
+        mandatory = True
+    )
+
+    file_key_type = cli.SwitchAttr(
+        "--file-key-type",
+        str,
+        help="The type of file key to use",
+        default = "Sha256e",
+    )
+
+    extension = cli.SwitchAttr(
+        "--extension",
+        str,
+        help="The file extension of the inserted record",
+        default = "txt",
+    )
+        
+    def main(self, *args) -> int:
+        if args:
+            print("This command does not take positional arguments")
+            return 1
+        asyncio.run(self._main_async())
+        return 0
+
+    async def _main_async(self):
+        base_config = self.parent.config
+        dataset_schema = DatasetSchema.must_load(self.dataset)
+
+        file_key_type = get_file_key_type(self.file_key_type)
+        BATCH_SIZE = 1000 # Arbitrary batch size for this command
+        async with Dataset.connect(base_config, BATCH_SIZE, dataset_schema) as dataset:
+            file_bytes = self.file_bytes.encode('utf-8')
+            key = file_key_type.from_bytes(file_bytes, self.extension)
+
+            key_columns = cast(TableRow, self.key_columns.split(','))
+            table = dataset.get_table(self.table_name)
+
+            await table.insert_file_source(key_columns, key, base_config.get_uuid())
+            print(f"Inserted row ({', '.join(key_columns)}, {key}) into table '{self.table_name}' in dataset '{self.dataset}'")
+        await base_config.get_filestore().put_file_bytes(file_bytes, key)
