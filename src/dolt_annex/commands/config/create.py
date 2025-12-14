5d594bca27fcca9c549e61df1e9a4da717ef1b75
Tobias Featherbutt <iamjohndoe220@gmail.com>
2025-12-14 03:34:06 -0800

    Add `dolt-annex create` subcommand for creating config obects (repos, datasets, etc)

---
+++ src/dolt_annex/commands/config/create.py
@@ -0,0 +1,46 @@
+import json
+import types
+from typing import Optional
+
+from dolt_annex.commands import SubCommand
+from dolt_annex.datatypes.loader import Loadable
+from dolt_annex.datatypes.remote import Repo
+from dolt_annex.datatypes.table import DatasetSchema
+    
+class Creator[T: Loadable]:
+    loadable_type: type[T]
+
+    def __init_subclass__(cls, loadable_type: type[T]) -> None:
+        cls.loadable_type = loadable_type
+
+    def main(self, name: str, value: str) -> int:
+        loadable_value = self.loadable_type(name=name, **json.loads(value))
+        loadable_value.save()
+        return 0
+
+LoadableTypes: dict[str, type[Loadable]] = {
+    "repo": Repo,
+    "dataset": DatasetSchema,
+}
+
+class Create(SubCommand):
+    """Create a new configuration object (repo, dataset, etc)."""
+
+    nested_command: Optional[Creator]
+
+    def main(self, *args) -> int:
+        if args:
+            print(f"Unknown command: create {args[0]}. Accepted values are: {', '.join(LoadableTypes.keys())}")
+            return 1
+        if self.nested_command is None:
+            self.help()
+            return 0
+        return 0
+
+for (name, loadable_type) in LoadableTypes.items():
+    CreateSubcommand = types.new_class(
+        f"Create{name.capitalize()}",
+        (Creator[loadable_type], SubCommand),
+        {"loadable_type": loadable_type}
+    )
+    Create.subcommand(name, CreateSubcommand)
---
+++ src/dolt_annex/commands/config/create_test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import json
+import pytest
+
+from dolt_annex.datatypes.loader import Loadable
+from dolt_annex.datatypes.remote import Repo
+from dolt_annex.datatypes.table import DatasetSchema
+from dolt_annex.test_util import run, setup
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("create_type,create_class,name,create_json", [
+    (
+        "repo",
+        Repo,
+        "foo_remote",
+        {
+            "uuid": "123e4567-e89b-12d3-a456-426614174000",
+            "filestore": {"type": "AnnexFS", "root": "."},
+            "key_format": "Sha256e"
+        }
+    ),
+    (
+        "dataset",
+        DatasetSchema,
+        "foo_dataset",
+        {
+            "tables": [
+                {
+                    "name": "test_table",
+                                "file_column": "file_key",
+                                "key_columns": ["path"]
+                }
+            ],
+            "empty_table_ref": "main"
+        }
+    ),
+])
+async def test_create_remote(tmp_path, create_class: type[Loadable], create_type, name, create_json):
+    async with setup(tmp_path):
+        # Use new Loadable context to unload created remote so we can test reloading it.
+        with Loadable.context():
+            await run(
+                args=["dolt-annex", "create", create_type, name, json.dumps(create_json)],
+            )
+        # Check that remote has been unloaded
+        assert name not in create_class.cache.get()
+        test_remote = create_class.must_load(name)
+        assert name in create_class.cache.get()
+        assert test_remote.model_dump(mode='json') == {"name": name, **create_json}
+
+@pytest.mark.asyncio
+async def test_create_invalid_type(tmp_path):
+    async with setup(tmp_path):
+        await run(
+            args=["dolt-annex", "create", "invalid", "name", "{}"],
+            expected_error_code=1,
+            expected_output="Unknown command: create invalid. Accepted values are: repo, dataset",
+        )
