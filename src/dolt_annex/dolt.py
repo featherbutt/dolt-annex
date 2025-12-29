6c079ee42f9f3ba9c89ec4c70874b3200e22d2a2
Tobias Featherbutt <iamjohndoe220@gmail.com>
2025-12-28 21:56:19 -0500

    Add DA_SERVER_ERR_LOGFILE environment variable for dumping the `dolt sql-server` stderr to a file.

--- src/dolt_annex/dolt.py
+++ src/dolt_annex/dolt.py
@@ -58,6 +58,8 @@ class DoltSqlServer:
             args.extend(["--socket", self.db_config["unix_socket"]])
         if (server_logfile := os.getenv("DA_SERVER_LOGFILE")) is not None:
             dolt = (dolt > server_logfile)
+        if (server_err_logfile := os.getenv("DA_SERVER_ERR_LOGFILE")) is not None:
+            dolt = (dolt >= server_err_logfile)
         dolt_server_process = dolt.popen(["sql-server", *args])
         while True:
             try:
