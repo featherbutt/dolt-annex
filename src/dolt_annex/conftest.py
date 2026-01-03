5d02e2826956aa0a28f71c435954a5481ca0fb6a
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-01-02 17:05:20 -0800

    Purge the Loadable cache after each test, to prevent accidental side effects of running tests.

--- src/dolt_annex/conftest.py
+++ src/dolt_annex/conftest.py
@@ -15,11 +15,12 @@ import pathlib
 import shutil
 import contextlib
 
-from plumbum import local
+from plumbum import local # type: ignore
 import pytest
 import pytest_asyncio
 
 from dolt_annex.data import data_dir
+from dolt_annex.datatypes.loader import Loadable
 from dolt_annex.datatypes.repo import Repo
 from dolt_annex.file_keys.sha256e import Sha256e
 from dolt_annex.test_util import create_test_filestore, local_uuid, remote_uuid, test_config, EnvironmentForTest
@@ -49,25 +50,27 @@ def init_dolt(dolt):
 @pytest_asyncio.fixture 
 async def setup(tmp_path: pathlib.Path, init_dolt):
 
-    local_filestore = await create_test_filestore("__local__", local_uuid, [])
-    remote_filestore = await create_test_filestore("test_remote", remote_uuid, [])
+    # Use Loadable.context to ensure that registered types will be reset at the end of the test.
+    with Loadable.context():
+        local_filestore = await create_test_filestore("__local__", local_uuid, [])
+        remote_filestore = await create_test_filestore("test_remote", remote_uuid, [])
 
-    Repo(
-        name="test_remote",
-        uuid=remote_uuid,
-        filestore=remote_filestore.file_store,
-        key_format=Sha256e
-    )
+        Repo(
+            name="test_remote",
+            uuid=remote_uuid,
+            filestore=remote_filestore.file_store,
+            key_format=Sha256e
+        )
 
-    with (tmp_path / "config.json").open("w") as f:
-        f.write(test_config.model_dump_json())
+        with (tmp_path / "config.json").open("w") as f:
+            f.write(test_config.model_dump_json())
 
-    async with (
-        local_filestore.open(test_config),
-        remote_filestore.open(test_config)
-    ):
-        with contextlib.chdir(tmp_path):
-            yield EnvironmentForTest(
-                local_file_store=local_filestore,
-                remote_file_store=remote_filestore,
-            )
+        async with (
+            local_filestore.open(test_config),
+            remote_filestore.open(test_config)
+        ):
+            with contextlib.chdir(tmp_path):
+                yield EnvironmentForTest(
+                    local_file_store=local_filestore,
+                    remote_file_store=remote_filestore,
+                )
