09b8261504fd9d12d1d73dc876f558a6d8830d1b
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-01-10 16:25:39 -0800

    Remove unused methods from filestore.cas

--- src/dolt_annex/filestore/cas.py
+++ src/dolt_annex/filestore/cas.py
@@ -2,13 +2,10 @@ from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
 from io import BytesIO
-from pathlib import Path
-from typing_extensions import Optional, AsyncContextManager, Self
+from typing_extensions import Optional, AsyncContextManager
 
 from dolt_annex.datatypes.async_utils import maybe_await
-from dolt_annex.datatypes.config import Config
-from dolt_annex.datatypes.file_io import ReadableFileObject
-from dolt_annex.datatypes.repo import Repo
+from dolt_annex.datatypes.file_io import Path, ReadableFileObject
 from dolt_annex.file_keys import FileKeyType
 from dolt_annex.file_keys.base import FileKey
 from dolt_annex.filestore.base import FileStore
@@ -21,42 +18,12 @@ class ContentAddressableStorage:
     _batch_size: Optional[int] = None
     _pending_changes: int = 0
 
-    @classmethod
-    def from_local(cls, config: Config) -> Self:
-        """Return a FileStore instance for the local repository."""
-        return cls(
-            file_store=config.get_filestore(),
-            file_key_format=config.default_file_key_type
-        )
-    
-    @classmethod
-    def from_remote(cls, remote: Repo) -> Self:
-        """Return a FileStore instance for this repository. The type of FileStore returned depends on the URL scheme"""
-        return cls(
-            file_store=remote.filestore,
-            file_key_format=remote.key_format
-        )
-
-    def open(self, config: 'Config') -> AsyncContextManager[Self]:
-        """
-        Open the underlying filestore for use.
-
-        Returns a context manager that yields the opened CAS instance.
-        """
-
-        @asynccontextmanager
-        async def open_cas() -> AsyncGenerator[Self]:
-            async with self.file_store.open(config) as fs:
-                yield self
-
-        return open_cas()
-
     async def tick(self) -> None:
         """Record a single operation in the current batch, then possibly flush."""
         if self._batch_size is not None:
             self._pending_changes += 1
             if self._pending_changes >= self._batch_size:
-                await self.file_store.flush()
+                await maybe_await(self.file_store.flush())
                 self._pending_changes = 0
 
     async def put_file(self, file_path: Path, file_key: Optional[FileKey] = None) -> FileKey:
@@ -90,7 +57,9 @@ class ContentAddressableStorage:
         """
         if file_key is None:
             file_key = self.file_key_format.from_bytes(file_bytes)
-        await maybe_await(self.file_store.put_file_object(BytesIO(file_bytes), file_key=file_key))
+        fd = BytesIO(file_bytes)
+        fd.name = str(file_key)
+        await maybe_await(self.file_store.put_file_object(fd, file_key=file_key))
         return file_key
 
     async def put_file_object(self, in_fd: ReadableFileObject, file_key: Optional[FileKey] = None) -> FileKey:
@@ -117,7 +86,7 @@ class ContentAddressableStorage:
             original_batch_size = self._batch_size
             self._batch_size = batch_size
             yield
-            await self.file_store.flush()
+            await maybe_await(self.file_store.flush())
             self._pending_changes = 0
             self._batch_size = original_batch_size
 
