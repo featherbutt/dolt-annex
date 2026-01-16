2743106de775ad207715f818fd90851de603b00f
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-01-16 13:58:42 -0800

    Update sync_test to take advantage of the new async file io

--- src/dolt_annex/sync/sync_test.py
+++ src/dolt_annex/sync/sync_test.py
@@ -1,85 +1,121 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pathlib
+from typing import Generator
 import uuid
 import pytest
-import tempfile
 import random
 
-import fs.osfs
+import pytest_asyncio
 
+from dolt_annex.datatypes.async_utils import maybe_await
 from dolt_annex.datatypes.common import TableRow
 from dolt_annex.datatypes.repo import Repo
 from dolt_annex.file_keys.sha256e import Sha256e
-from dolt_annex.filestore.annexfs import AnnexFS
+from dolt_annex.filestore.annexfs import AnnexFSModel
+from dolt_annex.filestore.archivefs import ArchiveFS, ArchiveFSModel
+from dolt_annex.filestore.base import FileStoreModel
 from dolt_annex.filestore.cas import ContentAddressableStorage
+from dolt_annex.filestore.filestore_test import SftpWrappedFilestoreModel, SimpleSftpFilestoreModel
+from dolt_annex.filestore.leveldb import LevelDBModel
 from dolt_annex.table import Dataset
 from dolt_annex.sync import move_dataset
 from dolt_annex.test_util import EnvironmentForTest, test_config, test_dataset_schema
 
-@pytest.mark.asyncio
-async def test_async_move(setup: EnvironmentForTest):
-    with (
-        tempfile.TemporaryDirectory() as from_dir, 
-        tempfile.TemporaryDirectory() as to_dir,
-    ):
-        from_filestore = AnnexFS(fs.osfs.OSFS(from_dir))
-        from_cas = ContentAddressableStorage(
-            from_filestore,
-            file_key_format=Sha256e,
+# Try every combination of two and from types.
+def all_filestore_types(prefix: pathlib.Path) -> Generator[FileStoreModel]:
+    yield LevelDBModel(root=prefix / "leveldb")
+    yield AnnexFSModel(root=prefix / "annexfs")
+    yield ArchiveFSModel(num_workers=4, root=prefix / "archivefs" / "archives", secondary=LevelDBModel(root=prefix / "archivefs" / "secondary"))
+    yield SftpWrappedFilestoreModel(
+        remote_file_store_model=ArchiveFSModel(
+            num_workers=4,
+            root=prefix / "archivefs" / "archives",
+            secondary=LevelDBModel(root=prefix / "archivefs" / "secondary")
         )
+    )
+    yield SimpleSftpFilestoreModel()
+
+def all_filestore_type_parameters(prefix: pathlib.Path):
+    for fs in all_filestore_types(prefix):
+        yield pytest.param(fs, id=fs.type_name())
+
+@pytest_asyncio.fixture(params=all_filestore_type_parameters(pathlib.Path("from")))
+async def from_repo_and_keys(request, temp_dir, base_config):
+    filestore_model: FileStoreModel = request.param
+    async with filestore_model.open(base_config) as filestore:
+        cas = ContentAddressableStorage(filestore, Sha256e)
         from_uuid = uuid.uuid4()
-        from_repo = Repo(
+        repo = Repo(
             "from_repo",
             from_uuid,
-            from_filestore,
-            Sha256e,
-        )
-        to_filestore = AnnexFS(fs.osfs.OSFS(to_dir))
-        to_uuid = uuid.uuid4()
-        to_repo = Repo(
-            "to_repo",
-            to_uuid,
-            to_filestore,
+            filestore,
             Sha256e,
         )
         # Create random files to move in parallel
-        NUM_FILES = 20
+        NUM_FILES = 5
         file_keys = []
         for _ in range(NUM_FILES):
             file_bytes = random.randbytes(1024**2) # 1 MB
-            file_key = await from_cas.put_file_bytes(file_bytes)
+            file_key = await cas.put_file_bytes(file_bytes)
             file_keys.append(file_key)
+        yield repo, file_keys
 
-        BATCH_SIZE = 1000 # Arbitrary batch size for this command
-        FILTERS = []
-        async with Dataset.connect(test_config, BATCH_SIZE, test_dataset_schema) as dataset:
-            # TODO: handle initializing branches automatically
-            dataset.dolt.initialize_dataset_source(test_dataset_schema, from_repo.uuid)
-            dataset.dolt.initialize_dataset_source(test_dataset_schema, to_repo.uuid)
-            # Add entries to from_repo database
-            table = dataset.get_table("test_table")
-            for i, file_key in enumerate(file_keys):
-                path = f"{i}"
-                await table.insert_file_source(
-                    TableRow((path,)),
-                    file_key,
-                    from_repo.uuid,
-                )
 
-            await table.flush()
-            await move_dataset(
-                dataset,
-                from_repo,
-                to_repo,
-                FILTERS,
+@pytest_asyncio.fixture(params=all_filestore_type_parameters(pathlib.Path("to")))
+async def to_repo(request, temp_dir, base_config):
+    filestore_model: FileStoreModel = request.param
+    async with filestore_model.open(base_config) as filestore:
+        to_uuid = uuid.uuid4()
+        repo = Repo(
+            "to_repo",
+            to_uuid,
+            filestore,
+            Sha256e,
+        )
+        yield repo
+
+
+@pytest.mark.asyncio
+async def test_async_move(setup: EnvironmentForTest, temp_dir: pathlib.Path, from_repo_and_keys: tuple[Repo, list[Sha256e]], to_repo: Repo):
+    from_repo, file_keys = from_repo_and_keys
+    BATCH_SIZE = 1000 # Arbitrary batch size for this command
+    FILTERS = [] # Allow for any setup delays
+    async with Dataset.connect(test_config, BATCH_SIZE, test_dataset_schema) as dataset:
+        # TODO: handle initializing branches automatically
+        dataset.dolt.initialize_dataset_source(test_dataset_schema, from_repo.uuid)
+        dataset.dolt.initialize_dataset_source(test_dataset_schema, to_repo.uuid)
+        # Add entries to from_repo database
+        table = dataset.get_table("test_table")
+        for i, file_key in enumerate(file_keys):
+            path = f"{i}"
+            await table.insert_file_source(
+                TableRow((path,)),
+                file_key,
+                from_repo.uuid,
             )
-            # Check that files have been moved
-            for file_key in file_keys:
-                assert to_filestore.exists(file_key)
-                assert await to_filestore.get_file_bytes(file_key) == await from_filestore.get_file_bytes(file_key)
-                # Check that db entries have been updated
-                for file_key, path in table.get_rows(to_repo.uuid, FILTERS):
-                    assert bytes(file_keys[int(path)]) == bytes(file_key, encoding='utf-8')
 
-                
+        await table.flush()
+        await move_dataset(
+            dataset,
+            from_repo,
+            to_repo,
+            FILTERS,
+        )
+        # Check that files have been moved
+        for file_key in file_keys:
+            assert await maybe_await(to_repo.filestore.exists(file_key))
+            assert await to_repo.filestore.get_file_bytes(file_key) == await from_repo.filestore.get_file_bytes(file_key)
+        # Check that db entries have been updated
+        for file_key, path in table.get_rows(to_repo.uuid, FILTERS):
+            assert bytes(file_keys[int(path)]) == bytes(file_key, encoding='utf-8')
+        # If the destination filestore is ArchiveFS, ensure that files are in multiple archives
+        if isinstance(to_repo.filestore, ArchiveFS):
+            archive_ids = set()
+            for file_key in file_keys:
+                file_location = await to_repo.filestore.secondary.get_file_bytes(file_key)
+                print(file_location)
+                name, offset, size = file_location.split(b":")
+                archive_ids.add(name)
+            # assert len(archive_ids) > 1
