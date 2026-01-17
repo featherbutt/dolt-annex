4f8d2dbe439d0ddc75e0790f8076ffc398ccc2ee
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-01-16 18:26:13 -0800

    Add `filestore export` command for printing files to stdout.

---
+++ src/dolt_annex/commands/filestore/export.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import aiofiles
+from plumbum import cli # type: ignore
+from dolt_annex.commands import CommandGroup, SubCommand
+
+from dolt_annex.datatypes.repo import RepoModel
+from dolt_annex.file_keys.base import FileKey
+from dolt_annex.filestore.base import copy
+
+class Export(SubCommand):
+    """Writes a file to stdout given its file key."""
+
+    parent: CommandGroup
+
+    file_key = cli.SwitchAttr(
+        "--file-key",
+        str,
+        help="The file key to look up. If unset, reads the file key from stdin.",
+    )
+
+    repo = cli.SwitchAttr(
+        "--repo",
+        str,
+        help="If set, read from this repo instead of the default",
+    )
+
+    async def main(self, *args) -> int:
+        if args:
+            print("This command does not take positional arguments")
+            return 1
+
+        file_key = self.file_key or sys.stdin.readline().strip()
+        
+        queried_key = FileKey.must_parse(bytes(file_key, encoding='utf-8'))
+        if self.repo:
+            repo = RepoModel.must_load(self.repo)
+        else:
+            repo = self.parent.config.get_default_repo()
+
+        async with repo.filestore.open(self.parent.config) as filestore:
+            async with filestore.with_file_object(queried_key) as f:
+                await copy(src=f.inner, dst=aiofiles.stdout_bytes)
+
+        return 0
---
+++ src/dolt_annex/commands/filestore/export_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from dolt_annex.file_keys.sha256e import Sha256e
+from dolt_annex.test_util import EnvironmentForTest, run
+
+@pytest.mark.asyncio
+async def test_export(tmp_path, setup: EnvironmentForTest):
+    """Run and validate pushing content files to a remote"""
+
+    key = Sha256e.from_bytes(b"new file content", "txt")
+    await setup.local_file_store.put_file_bytes(b"new file content", key)
+    await run(
+        args=["dolt-annex", "filestore", "export-file", "--file-key", str(key)],
+        expected_output_contains="new file content"
+    )
+
+@pytest.mark.asyncio
+async def test_filekey_from_stdin(tmp_path, setup: EnvironmentForTest):
+    """Run and validate pushing content files to a remote"""
+
+    key = Sha256e.from_bytes(b"new file content", "txt")
+    await setup.local_file_store.put_file_bytes(b"new file content", key)
+    await run(
+        args=["dolt-annex", "filestore", "export-file"],
+        stdin=str(key) + "\n",
+        expected_output_contains="new file content"
+    )
