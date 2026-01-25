342ae57bde69f7a14cc3e57baadd0f70fd036c5f
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-01-25 14:56:17 -0800

    Initial diff command

---
+++ src/dolt_annex/commands/dataset/diff.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from typing_extensions import List
+
+from plumbum import cli
+
+from dolt_annex.application import Application
+from dolt_annex.datatypes.config import Config
+from dolt_annex.datatypes.table import DatasetSchema
+from dolt_annex.table import Dataset, TableFilter
+from dolt_annex.datatypes.repo import RepoModel
+from dolt_annex.sync import diff_keys
+
+class Diff(cli.Application):
+    """Print records that differ between two versions of a dataset."""
+
+    parent: Application
+
+    limit = cli.SwitchAttr(
+        "--limit",
+        int,
+        help="The maximum number of records to print",
+        default = None,
+    )
+
+    from_repo = cli.SwitchAttr(
+        "--from",
+        str,
+        help="The first repo to compare",
+    )
+
+    to_repo = cli.SwitchAttr(
+        "--to",
+        str,
+        help="The second repo to compare",
+    )
+
+    dataset = cli.SwitchAttr(
+        "--dataset",
+        str,
+        help="The name of the dataset being compared",
+    )
+
+    table_name = cli.SwitchAttr(
+        "--table",
+        str,
+        help="The name of the table in the dataset being compared",
+        mandatory=True,
+    )
+
+    @cli.switch(
+        "--where",
+        str,
+        list = True,
+        help="A filter condition on the table rows to be pushed",
+    )
+    def where(self, filter_strings: List[str]):
+        for filter_string in filter_strings:
+            if '=' not in filter_string:
+                raise ValueError(f"Invalid filter string: {filter_string}")
+            column_name, column_value = filter_string.split('=', maxsplit=1)
+            self.filters.append(TableFilter(column_name, column_value))
+
+    filters: List[TableFilter] = []
+
+    async def main(self, *args: list[str]) -> int:
+        """Entrypoint for pull command"""
+        base_config: Config = self.parent.config
+
+        dataset_schema = DatasetSchema.must_load(self.dataset)
+        BATCH_SIZE = 1000 # Arbitrary batch size for this command, unused
+        async with (
+            Dataset.connect(base_config, BATCH_SIZE, dataset_schema) as dataset
+        ):
+            local_repo_model = RepoModel.must_load(self.from_repo)
+            remote_repo_model = RepoModel.must_load(self.to_repo)
+            dataset.dolt.initialize_dataset_source(dataset_schema, local_repo_model.uuid)
+            dataset.dolt.initialize_dataset_source(dataset_schema, remote_repo_model.uuid)
+            dolt = dataset.dolt
+            table_schema = dataset_schema.get_table(self.table_name)
+            keys_and_submissions = list(diff_keys(dolt, str(local_repo_model.uuid), str(remote_repo_model.uuid), dataset.name, table_schema, self.filters, self.limit))
+            for diff_type, key, submission in keys_and_submissions:
+                    print(",".join([diff_type, str(key), str(submission)]))
+        return 0
