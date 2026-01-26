4dc3882c646b65cdcd302c56899cda7858cf7a0f
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-01-25 16:35:39 -0800

    Add cli test for `dataset diff` and `filestore whereis`

--- scripts/cli_tests.sh
+++ scripts/cli_tests.sh
@@ -13,6 +13,12 @@ cd $temp_dir
 
 dolt-annex init
 
+dolt-annex create repo remote '{
+            "uuid": "123e4567-e89b-12d3-a456-426614174000",
+            "filestore": {"type": "annexfs", "root": "."},
+            "key_format": "Sha256e"
+        }'
+
 output=$(dolt-annex gallery-dl https://www.furaffinity.net/view/63142315/)
 assert_eq $(echo $output | jq '.submission_files_processed') 1
 assert_eq $(echo $output | jq '.submission_metadata_files_processed') 0
@@ -30,5 +36,14 @@ dolt-annex dataset read-table \
 
 actual_hash=$(sha256sum downloaded_image.png | awk '{print $1}')
 assert_eq "$actual_hash" "28c9485eec3f2e33fa7c0f3c7a5ae62f94e939f3a494e4c5e7dfd16d8c8776c7"
+
+output=$(dolt-annex dataset diff \
+  --dataset gallery-dl \
+  --table submissions \
+  --from __local__ \
+  --to remote \
+  | cut -d , -f 2 \
+  | dolt-annex filestore whereis)
+assert_contain "$output" '"name": "__local__"'
 
 log_success "All tests passed!"
