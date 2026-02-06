62fc9048c20ccc0b2c1a185b802d89a638d30bc2
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-02-06 12:54:08 -0800

    Bump version to 0.6.0

--- src/dolt_annex/application.py
+++ src/dolt_annex/application.py
@@ -30,7 +30,7 @@ default_config_file_locations = [
 class Application(CommandGroup):
     """The top level CLI command"""
     PROGNAME = "dolt-annex"
-    VERSION = "0.5.0"
+    VERSION = "0.6.0"
 
     config_file = cli.SwitchAttr(['-c', '--config'], cli.ExistingFile, envname=Env.CONFIG_FILE)
 
