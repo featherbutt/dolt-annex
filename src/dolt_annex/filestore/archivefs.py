ed56e69b5f12863cccc77e86d4daed748512c4e4
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-02-15 13:52:02 -0800

    Implement filelocks on archivefs and rotate archive files when they exceed the max size.

--- src/dolt_annex/filestore/archivefs.py
+++ src/dolt_annex/filestore/archivefs.py
@@ -9,10 +9,12 @@ with the file key as the key and the file contents as the value.
 from __future__ import annotations
 
 import asyncio
-from contextlib import asynccontextmanager
+from contextlib import asynccontextmanager, contextmanager
 import pathlib
 import tarfile
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Generator, Optional
+import uuid
+from filelock import FileLock, Timeout
 from pydantic import InstanceOf
 from typing_extensions import override, Tuple
 
@@ -25,7 +27,7 @@ from dolt_annex.datatypes.config import Config
 from dolt_annex.datatypes.file_io import Path, async_open
 from dolt_annex.file_keys import FileKey
 from dolt_annex.filestore.file_handles import ExistingFileHandle
-from dolt_annex.tarfile import addfile
+from dolt_annex.tarfile_utils import addfile, advance_to_end
 
 from .base import FileInfo, FileStore, FileStoreModel
 
@@ -42,6 +44,11 @@ class ArchiveFS(FileStore):
     secondary: FileStore
     files_queue: asyncio.Queue[Tuple[FileKey, AsyncContextManager[ReadableStream], asyncio.Future[None]]]
     workers: asyncio.TaskGroup
+    max_archive_size: int
+
+    writable_archives_dir: Path
+    finalized_archives_dir: Path
+    locks_dir: Path
 
     def __init__(
             self, *,
@@ -54,53 +61,94 @@ class ArchiveFS(FileStore):
         self.file_system = file_system
         self.secondary = secondary
         self.workers = workers
-        # Iterate over the current "hot" archive files, assigning each one to a worker.
+        self.max_archive_size = max_archive_size
+        
+        self.writable_archives_dir = Path(self.file_system, "writable_archives")
+        self.finalized_archives_dir = Path(self.file_system, "finalized_archives")
+        self.locks_dir = Path(self.file_system, "locks")
+
+        self.writable_archives_dir.mkdirs(exist_ok=True)
+        self.finalized_archives_dir.mkdirs(exist_ok=True)
+        self.locks_dir.mkdirs(exist_ok=True)
+
         self.files_queue = asyncio.Queue()
-        for i in range(num_workers):
-            archive_file = Path(file_system) / f"archive_{i}.tar"
-            # TODO: Rotate archive files if they exceed max_archive_size.
-            workers.create_task(self._worker_loop(archive_file))
+        for _ in range(num_workers):
+            workers.create_task(self._worker_loop())
 
-    async def _worker_loop(self, archive_file: Path) -> None:
-        # TODO: If the application previously terminated suddenly, the archive may
-        # end with an incomplete entry. This is fine. We can seek to the start of
-        # the incomplete entry, and it will be overwritten by the next insert.
-        if not archive_file.exists() or archive_file.stat().size == 0:
-            archive_fd_sync = archive_file.open_sync('wb')
-            archive_tar = tarfile.open(fileobj=archive_fd_sync, mode='w')
-        else:
-            archive_fd_sync = archive_file.open_sync('r+b')
-            archive_tar = tarfile.open(fileobj=archive_fd_sync, mode='a')
-        async with async_open(archive_fd_sync) as archive_fd:
-            with archive_tar:
-                while True:
-                    try:
-                        file_key, data_source, callback = await self.files_queue.get()
-                    except asyncio.QueueShutDown:
-                        break
-                    try:
-                        tar_info = tarfile.TarInfo(name=str(file_key))
-                        tar_info.size = file_key.size
-                        buf = tar_info.tobuf(archive_tar.format, archive_tar.encoding, archive_tar.errors)
-                        offset = archive_tar.offset + len(buf)
-                        # TODO: Rotate archive files if they exceed max_archive_size.
-                        async with data_source as in_fd:
-                            await addfile(archive_tar, tarfile_fd=archive_fd, tarinfo=tar_info, input_fileobj=in_fd)
-                        # TODO: We probably don't need to flush immediately after each write,
-                        # But there's no way to signal a flush for tests.
-                        archive_fd_sync.flush()
-                        secondary_value = f"{archive_file.name}:{offset}:{tar_info.size}"
-                        await maybe_await(self.secondary.put_file_bytes(secondary_value.encode('utf-8'), file_key))
-                        callback.set_result(None)
-                    except Exception as e:
-                        callback.set_exception(e)
-                    finally:
-                        self.files_queue.task_done()
+    @contextmanager
+    def open_archive_file_for_write(self, archive_file_path: Path) -> Generator[Optional[tarfile.TarFile]]:
+        lock_path = self.locks_dir / f"{archive_file_path.name}.lock"
+        lock_syspath = lock_path.getsyspath()
+        assert lock_syspath is not None, "ArchiveFS requires a local filesystem that supports file locks."
+        try:
+            lock = FileLock(lock_syspath, blocking=False)
+            with lock:
+                archive_file_path.touch()
+                archive_fd_sync = archive_file_path.open_sync('r+b')
+                archive_tar = tarfile.open(fileobj=archive_fd_sync, mode='w')
+                yield archive_tar
+        except Timeout:
+            # The lock is held by another process, so we cannot access this archive file.
+            yield None
 
+    @contextmanager
+    def get_archive_file_for_write(self) -> Generator[Tuple[tarfile.TarFile, Path], None, None]:
+        # Attempt to acquire a lock on a "hot" archive file.
+        for archive_file in self.writable_archives_dir.children():
+            with self.open_archive_file_for_write(archive_file) as archive_tar:
+                if archive_tar is not None:
+                    yield archive_tar, archive_file
+                    return
+        
+        # If we cannot acquire a lock on any existing writable archive files, create a new one.
+        while True:
+            new_archive_file_name = f"{uuid.uuid7()}.tar"
+            new_archive_file_path = self.writable_archives_dir / new_archive_file_name
+            with self.open_archive_file_for_write(new_archive_file_path) as archive_tar:
+                if archive_tar is not None:
+                    yield archive_tar, new_archive_file_path
+                    return
 
+    async def _worker_loop(self) -> None:
+        while True:
+            with self.get_archive_file_for_write() as (archive_tar, archive_file):
+                archive_fd_sync = archive_tar.fileobj
+                # assert isinstance(archive_fd_sync, BinaryIO), "Failed to get file object from archive_tar."
+                async with async_open(archive_fd_sync) as archive_fd:
+                    with archive_tar:
+                        advance_to_end(archive_tar)
+                        while archive_tar.offset < self.max_archive_size:
+                            try:
+                                file_key, data_source, callback = await self.files_queue.get()
+                            except asyncio.QueueShutDown:
+                                break
+                            try:
+                                tar_info = tarfile.TarInfo(name=str(file_key))
+                                tar_info.size = file_key.size
+                                buf = tar_info.tobuf(archive_tar.format, archive_tar.encoding, archive_tar.errors)
+                                offset = archive_tar.offset + len(buf)
+                                async with data_source as in_fd:
+                                    await addfile(archive_tar, tarfile_fd=archive_fd, tarinfo=tar_info, input_fileobj=in_fd)
+                                # TODO: We probably don't need to flush immediately after each write,
+                                # But there's no way to signal a flush for tests.
+                                archive_fd_sync.flush()
+                                secondary_value = f"{archive_file.name}:{offset}:{tar_info.size}"
+                                await maybe_await(self.secondary.put_file_bytes(secondary_value.encode('utf-8'), file_key))
+                                callback.set_result(None)
+                            except Exception as e:
+                                callback.set_exception(e)
+                            finally:
+                                self.files_queue.task_done()
+            # Move the archive file to the finalized directory so that it is no longer used for writing.
+            archive_file.rename(self.finalized_archives_dir / archive_file.name)
 
     @override
     async def put_file_object(self, data_source: AsyncContextManager[ReadableStream], file_key: FileKey) -> Result[None]:
+        if await maybe_await(self.secondary.exists(file_key)):
+            # The file already exists, so we don't need to do anything.
+            # Open the stream in order to close it.
+            async with data_source:
+                return Result.done()
         callback = asyncio.Future[None]()
         await self.files_queue.put((file_key, data_source, callback))
         return Result(callback)
@@ -184,3 +232,5 @@ class ArchiveFSModel(FileStoreModel):
     def type_name(self) -> str:
         """Get the type name of the filestore. Used in tests."""
         return f"ArchiveFS({self.secondary.type_name()})"
+
+
--- src/dolt_annex/tarfile_utils.py
+++ src/dolt_annex/tarfile_utils.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-``
 
-from tarfile import TarFile, TarInfo, BLOCKSIZE, NUL
+from tarfile import HeaderError, TarFile, TarInfo, BLOCKSIZE, NUL
 
 from dolt_annex.datatypes.async_types import ReadableStream, WritableFileObject
 from dolt_annex.filestore.base import copy
+
+def advance_to_end(tarfile: TarFile):
+    """
+    The standard library tarfile module raises an exception and closes if
+    the file does not end with two empty blocks after the last member.
+
+    We want to be more permissive, which means manually advancing to the end of the archive file.
+    If we encounter an unexpected EOF or if the file ends with a non-empty block,
+    we simply seek to the end of the last member and continue writing from there.
+    """
+    while True:
+        tarfile.fileobj.seek(tarfile.offset)
+        previous_offset = tarfile.offset
+        try:
+            tarinfo = tarfile.tarinfo.fromtarfile(tarfile)
+            # Calling fromtarfile advances the offset to the start of the next block.
+            # Check that the file is not truncated by attempting to read the last byte of the previous block.
+            tarfile.fileobj.seek(tarfile.offset - 1)
+            if tarfile.fileobj.read(1) == b'':
+                tarfile.fileobj.seek(previous_offset)
+                break
+            tarfile.members.append(tarinfo)
+        except HeaderError:
+            tarfile.fileobj.seek(tarfile.offset)
+            break
 
 async def addfile(tarfile: TarFile, tarinfo: TarInfo, tarfile_fd: WritableFileObject, input_fileobj: ReadableStream):
     """Add the TarInfo object 'tarinfo' to the archive. If 'tarinfo' represents
