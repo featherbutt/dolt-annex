94cf66193e783fc44737f8ff8533b2abeba05acd
featherbutt <iamjohndoe220@gmail.com>
2026-03-22 19:54:59 -0700

    Add Subscribestar source to map of sources

--- src/dolt_annex/gallery_dl_plugin/sources/__init__.py
+++ src/dolt_annex/gallery_dl_plugin/sources/__init__.py
@@ -11,6 +11,7 @@ from .ao3 import AO3
 from .pixiv import Pixiv
 from .nhentai import NHentai
 from .e621 import E621
+from .subscribestar import SubscribeStar
 
 category_to_source : Dict[str, GalleryDLSource]= {
     "itaku": Itaku(),
@@ -20,6 +21,7 @@ category_to_source : Dict[str, GalleryDLSource]= {
     "inkbunny": Inkbunny(),
     "nhentai": NHentai(),
     "e621": E621(),
+    "subscribestar": SubscribeStar()
 }
 
 def get_source(category: str, subcategory: str) -> GalleryDLSource:
