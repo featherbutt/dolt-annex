bba641820d1cb12792f7ba9016fd118c0c4d8198
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-03-29 11:05:32 -0700

    Skip filestore verification test for SftpFileStore, since the interface doesn't let us put an incorrect file.

--- src/dolt_annex/filestore/filestore_test.py
+++ src/dolt_annex/filestore/filestore_test.py
@@ -160,11 +160,14 @@ async def test_file_stores(cas: ContentAddressableStorage):
         await cas.file_store.verify_file(key)
 
     # Check that a file with the incorrect key does not verify
-    wrong_sha256_key = Sha256E.from_bytes(b"wrong bytes")
-    result = await maybe_await(cas.file_store.put_file_bytes(file_bytes, wrong_sha256_key))
-    await result.wait_for_complete()
-    with pytest.RaisesGroup(AssertionError, flatten_subgroups=True, allow_unwrapped=True):
-        await cas.file_store.verify_file(wrong_sha256_key)
+    # SftpFileStore has additional checks and won't let us put an incorrect file in the first place,
+    # So we skip this test for SftpFileStore.
+    if not isinstance(cas.file_store, SftpFileStore):
+        wrong_sha256_key = Sha256E.from_bytes(b"wrong bytes")
+        result = await maybe_await(cas.file_store.put_file_bytes(file_bytes, wrong_sha256_key))
+        await result.wait_for_complete()
+        with pytest.RaisesGroup(AssertionError, flatten_subgroups=True, allow_unwrapped=True):
+            await cas.file_store.verify_file(wrong_sha256_key)
 
     # Check that exist for non-existent file returns false
     assert not await maybe_await(cas.file_store.exists(Sha256E.from_bytes(b"nonexistent")))
