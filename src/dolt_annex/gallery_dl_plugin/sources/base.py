d08cd1614061635cfe4c9e6c4621082befec248c
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-04-17 13:22:58 -0700

    Allow removing subfields from metadata objects in gallery-dl

--- src/dolt_annex/gallery_dl_plugin/sources/base.py
+++ src/dolt_annex/gallery_dl_plugin/sources/base.py
@@ -7,6 +7,10 @@ from typing_extensions import Any, Iterable
 
 from dolt_annex.datatypes.common import TableRow
 
+type SequencePathSelector = 'tuple[str | UnionPathSelector, ...]'
+type UnionPathSelector = 'list[str | SequencePathSelector]'
+type PathSelector = str | SequencePathSelector | UnionPathSelector
+
 def is_private_field(field: str) -> bool:
     """Whether a field is considered private and should be excluded from imported metadata."""
     return field.startswith("_")
@@ -41,7 +45,7 @@ class GalleryDLSource:
         return is_private_field(field) or field == "subcategory" or field in self.fields_to_remove()
 
     @abstractmethod
-    def fields_to_remove(self) -> list[str | list[str]]:
+    def fields_to_remove(self) -> PathSelector:
         """
         A list of fields that will be removed from the imported metadata.
         
@@ -60,7 +64,6 @@ class GalleryDLSource:
         metadata["_id"] = self.id(metadata)
         metadata["_date"] = self.updated_date(metadata)
 
-    @abstractmethod
     def post_metadata(self, metadata: dict[str, Any]) -> Iterable[TableRow]:
         return [TableRow(( self.source_name, metadata["_id"], metadata["_metadata_file_key"]))]
 
@@ -84,31 +87,49 @@ class GalleryDLSource:
         """
         return metadata.get("num", 1)
 
-
-
-
-def mutate_remove_field(d: dict | list, field_to_remove: str | list[str]):
+def mutate_remove_fields(d: dict | list, field_to_remove: PathSelector):
+    """
+    Modifies a JSON object to remove all fields matching the provided selector.
+    """
     if isinstance(d, list):
+        # If the input object is a list, simply apply the selector to each of its elements.
         for item in d:
-            mutate_remove_field(item, field_to_remove)
+            mutate_remove_fields(item, field_to_remove)
         return
     
+    # Here, |d| must be a dict.
     if isinstance(field_to_remove, str):
         if field_to_remove in d:
             del d[field_to_remove]
         return
     
+    # |field_to_remove| is either a UnionPathSelector or a SequencePathSelector
+    # In either case, if it only has a single child selector, recurse on that child.
     if len(field_to_remove) == 1:
-        if field_to_remove[0] in d:
-            del d[field_to_remove[0]]
+        return mutate_remove_fields(d, next(iter(field_to_remove)))
+        
+    if isinstance(field_to_remove, list):
+        # The selector is a UnionPathSelector: recursively apply each selector.
+        for field in field_to_remove:
+            mutate_remove_fields(d, field)
         return
 
-    field, *rest = field_to_remove
+    # Else the selector is a SequencePathSelector
+    # Don't use destructuring syntax here because it would make rest a list.
+    field, rest = field_to_remove[0], field_to_remove[1:]
+        
+    if isinstance(field, str):
+        if field in d:
+            mutate_remove_fields(d[field], rest)
+        return
 
-    if field in d:
-        mutate_remove_field(d[field], rest)
-
-def mutate_remove_fields(d: dict, fields_to_remove: list[str | list[str]]):
-    """Remove fields from a dictionary in place."""
-    for field in fields_to_remove:
-        mutate_remove_field(d, field)
+    # field is a UnionPathSelector
+    for possible_field in field:
+        if isinstance(possible_field, str):
+            if possible_field in d:
+                mutate_remove_fields(d[possible_field], rest)
+            return
+        # possible_field is a SequencePathSelector
+        child_rest = (*possible_field[1:], *rest)
+        possible_field = possible_field[0]
+        mutate_remove_fields(d[possible_field], child_rest)
--- src/dolt_annex/gallery_dl_plugin/sources/inkbunny.py
+++ src/dolt_annex/gallery_dl_plugin/sources/inkbunny.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing_extensions import Any, Iterable, override
 
-from dolt_annex.datatypes.common import TableRow
-from .base import GalleryDLSource
+from typing_extensions import Any, override
+
+from .base import GalleryDLSource, UnionPathSelector
 
 class Inkbunny(GalleryDLSource, source_name = "inkbunny.net"):
     """Support for inkbunny.net"""
@@ -14,7 +14,7 @@ class Inkbunny(GalleryDLSource, source_name = "inkbunny.net"):
         return ["post", "user", "search"]
     
     @override
-    def fields_to_remove(self) -> list[str | list[str]]:
+    def fields_to_remove(self) -> UnionPathSelector:
         return [
             "comments_count",
             "favorites_count",
@@ -27,8 +27,52 @@ class Inkbunny(GalleryDLSource, source_name = "inkbunny.net"):
             "user_icon_url_medium",
             "user_icon_url_small",
             "search",
+            "last_file_update_datetime_usertime",
+            "create_datetime_usertime",
+            ( "pools", [
+                "submission_left_file_name",
+                "submission_left_submission_id",
+                "submission_left_thumb_huge_x",
+                "submission_left_thumb_huge_y",
+                "submission_left_thumb_large_x",
+                "submission_left_thumb_large_y",
+                "submission_left_thumb_medium_x",
+                "submission_left_thumb_medium_y",
+                "submission_left_thumb_huge_noncustom_x",
+                "submission_left_thumb_huge_noncustom_y",
+                "submission_left_thumb_large_noncustom_x",
+                "submission_left_thumb_large_noncustom_y",
+                "submission_left_thumb_medium_noncustom_x",
+                "submission_left_thumb_medium_noncustom_y",
+                "submission_left_thumbnail_url_huge",
+                "submission_left_thumbnail_url_huge_noncustom",
+                "submission_left_thumbnail_url_large",
+                "submission_left_thumbnail_url_large_noncustom",
+                "submission_left_thumbnail_url_medium",
+                "submission_left_thumbnail_url_medium_noncustom",
+                "submission_right_file_name",
+                "submission_right_submission_id",
+                "submission_right_thumb_huge_x",
+                "submission_right_thumb_huge_y",
+                "submission_right_thumb_large_x",
+                "submission_right_thumb_large_y",
+                "submission_right_thumb_medium_x",
+                "submission_right_thumb_medium_y",
+                "submission_right_thumb_huge_noncustom_x",
+                "submission_right_thumb_huge_noncustom_y",
+                "submission_right_thumb_large_noncustom_x",
+                "submission_right_thumb_large_noncustom_y",
+                "submission_right_thumb_medium_noncustom_x",
+                "submission_right_thumb_medium_noncustom_y",
+                "submission_right_thumbnail_url_huge",
+                "submission_right_thumbnail_url_huge_noncustom",
+                "submission_right_thumbnail_url_large",
+                "submission_right_thumbnail_url_large_noncustom",
+                "submission_right_thumbnail_url_medium",
+                "submission_right_thumbnail_url_medium_noncustom",
+            ])
         ]
-    
+
     @override
     def id(self, metadata: dict[str, Any]) -> str:
         """A unique identifier for the post."""
