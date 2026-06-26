90d6e5e4df457e64b0c4f7008bf4a9cf830585cf
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-06-25 18:08:55 -0700

    Add --all flag to `dolt-annex filestore copy`

--- src/dolt_annex/commands/filestore/copy.py
+++ src/dolt_annex/commands/filestore/copy.py
@@ -32,21 +32,32 @@ class Copy(SubCommand):
         help="The repo to copy files to",
         mandatory=True
     )
+
+    all = cli.Flag(
+        "--all",
+        help="If set, copy all files from the source repo to the destination repo.",
+        default=False,
+    )
         
     async def main(self, *args) -> int:
-
         async with (
             Repo.open(self.config, self.from_repo) as from_repo,
             Repo.open(self.config, self.to_repo) as to_repo,
         ):
-            for file_key in args or sys.stdin.readlines():
-                queried_key = FileKey.must_parse(bytes(file_key.strip(), encoding='utf-8'))
-                result = await filestore_copy(
-                    src=from_repo.filestore,
-                    dst=to_repo.filestore,
-                    key=queried_key
-                )
-                await result.wait_for_complete()
-                logger.info("Copied key %s from repo %s to repo %s", queried_key, from_repo.name, to_repo.name)
-        
+            if self.all:
+                async for file_key, data_source in from_repo.filestore.file_store.get_files():
+                    result = await to_repo.filestore.put_file_object(data_source, file_key=file_key)
+                    await result.wait_for_complete()
+                    logger.info("Copied key %s from repo %s to repo %s", file_key, from_repo.name, to_repo.name)
+            else:
+                for key_str in args or sys.stdin.readlines():
+                    file_key = FileKey.must_parse(bytes(key_str.strip(), encoding='utf-8'))
+                    result = await filestore_copy(
+                        src=from_repo.filestore,
+                        dst=to_repo.filestore,
+                        key=file_key,
+                    )
+                    await result.wait_for_complete()
+                    logger.info("Copied key %s from repo %s to repo %s", file_key, from_repo.name, to_repo.name)
+
         return 0
