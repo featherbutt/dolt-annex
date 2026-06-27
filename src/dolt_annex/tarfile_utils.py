2ef18b45d54a905d1167f272a84439ecf1fb39d1
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-06-27 10:01:50 -0700

    Refactor FileStore API

--- src/dolt_annex/tarfile_utils.py
+++ src/dolt_annex/tarfile_utils.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-``
 
+from dataclasses import dataclass
 from tarfile import HeaderError, TarFile, TarInfo, BLOCKSIZE, NUL
+from typing import Callable, NamedTuple
 
 from dolt_annex.datatypes.async_types import ReadableStream, WritableFileObject
+from dolt_annex.file_keys.base import FileKey
 from dolt_annex.filestore.base import copy
 
 def advance_to_end(tarfile: TarFile):
@@ -31,21 +34,47 @@ def advance_to_end(tarfile: TarFile):
             tarfile.fileobj.seek(tarfile.offset)
             break
 
-async def addfile(tarfile: TarFile, tarinfo: TarInfo, tarfile_fd: WritableFileObject, input_fileobj: ReadableStream):
+class TarFileEntry(NamedTuple):
+    file_key: FileKey
+    offset: int
+    size: int
+
+async def addfile(tarfile: TarFile, tarfile_fd: WritableFileObject, input_fileobj: ReadableStream, file_key_producer: Callable[[], FileKey]) -> TarFileEntry:
     """Add the TarInfo object 'tarinfo' to the archive. If 'tarinfo' represents
         a non zero-size regular file, the 'fileobj' argument should be a binary file,
         and tarinfo.size bytes are read from it and added to the archive.
         You can create TarInfo objects directly, or by using gettarinfo().
     """
-    buf = tarinfo.tobuf(tarfile.format, tarfile.encoding, tarfile.errors)
-    await tarfile_fd.write(buf)
-    tarfile.offset += len(buf)
-
-    await copy(src=input_fileobj, dst=tarfile_fd)
-    blocks, remainder = divmod(tarinfo.size, BLOCKSIZE)
+    # Since we don't know the file size ahead of time, write the contents first, then write the header.
+    # To account for different tar formats, we create a dummy header with the correct size, write the contents, then write the actual header.
+    # As a precaution, we verify that the actual header has the same size as the dummy header.
+    dummy_tar_info = TarInfo()
+    header_size = len(dummy_tar_info.tobuf(tarfile.format, tarfile.encoding, tarfile.errors))
+    header_offset = tarfile.offset
+    file_offset = header_offset + header_size
+    tarfile.offset = file_offset
+    await tarfile_fd.seek(tarfile.offset)
+    file_size = await copy(src=input_fileobj, dst=tarfile_fd)
+    blocks, remainder = divmod(file_size, BLOCKSIZE)
     if remainder > 0:
         await tarfile_fd.write(NUL * (BLOCKSIZE - remainder))
         blocks += 1
-    tarfile.offset += blocks * BLOCKSIZE
+    final_offset = file_offset + blocks * BLOCKSIZE
+    # Now write the header
+    file_key = file_key_producer()
+    tarfile.offset = header_offset
+    await tarfile_fd.seek(tarfile.offset)
+    tar_info = TarInfo(name=str(file_key))
+    tar_info.size = file_size
+    buf = tar_info.tobuf(tarfile.format, tarfile.encoding, tarfile.errors)
+    assert len(buf) == header_size, "Header size mismatch"
+    await tarfile_fd.write(buf)
+    tarfile.offset = final_offset
+    await tarfile_fd.seek(tarfile.offset)
 
-    tarfile.members.append(tarinfo)
+    tarfile.members.append(tar_info)
+    return TarFileEntry(
+        file_key=file_key,
+        offset=file_offset,
+        size=file_size
+    )
