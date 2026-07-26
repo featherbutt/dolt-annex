99bc74cd7295bc49469d3543b2a7504329b2be9f
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-07-26 16:57:20 -0700

    Add test that gallery_dl key type migration script handles the case where only the submission file needs to be updated.

--- src/dolt_annex/scripts/gallery_dl_alter_file_key_type_test.py
+++ src/dolt_annex/scripts/gallery_dl_alter_file_key_type_test.py
@@ -9,6 +9,7 @@ import fs.memoryfs
 import pytest
 
 from dolt_annex.datatypes.async_utils import as_acm
+from dolt_annex.datatypes.common import TableRow
 from dolt_annex.datatypes.repo import RepoModel
 from dolt_annex.datatypes.table import DatasetSchema
 from dolt_annex.file_keys import Sha256E
@@ -30,14 +31,36 @@ async def test_alter_file_key_type(tmp_path, setup: EnvironmentForTest):
     await run(
         args=["dolt-annex", "gallery-dl", "https://www.furaffinity.net/view/63142315/"],
     )
+
+    dataset_schema = DatasetSchema.must_load("gallery-dl")
+
+    async with (
+        as_acm(DatabaseConnection.open(setup.config)) as conn,
+        as_acm(conn.open_dataset(dataset_schema)) as dataset,
+        dataset.with_repo(setup.local_repo.uuid) as dataset_repo,
+    ):
+        metadata_table = dataset_repo.get_table("metadata")
+        submissions_table = dataset_repo.get_table("submissions")
+
+        # To test the outcome where only the submission needs to be rewritten,
+        # insert an extra row into the submissions table.
+        extra_metadata_key = Sha256HSe.from_bytes(b"metadata")
+        extra_submission_key = Sha256E.from_bytes(b"submission")
+        await submissions_table.insert(TableRow({
+            "source": "test",
+            "id": 1,
+            "metadata_file_key": bytes(extra_metadata_key),
+            "part": 1,
+            "submission_file_key": bytes(extra_submission_key)
+        }))
+        await submissions_table.flush()
+
     await run(
         args=[
             "dolt-annex", "script", "gallery_dl_alter_file_key_type", "--",
             "--file-key-type", "SHA256_HSe",
         ],
     )
-
-    dataset_schema = DatasetSchema.must_load("gallery-dl")
 
     async with (
         as_acm(DatabaseConnection.open(setup.config)) as conn,
@@ -53,7 +76,7 @@ async def test_alter_file_key_type(tmp_path, setup: EnvironmentForTest):
             assert type(file_key) is Sha256HSe
             await setup.local_repo.filestore.verify_file(file_key)
 
-        assert len(list(submissions_table.get_rows())) == 1
+        assert len(list(submissions_table.get_rows())) == 2
         for row in submissions_table.get_rows():
             submission_file_key = FileKey.must_parse(row["submission_file_key"])
             metadata_file_key = FileKey.must_parse(row["metadata_file_key"])
