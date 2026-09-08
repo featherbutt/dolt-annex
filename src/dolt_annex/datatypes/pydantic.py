45090511cd1eee759c2b18f4b701aa4db323fee4
Tobias Featherbutt <iamjohndoe220@gmail.com>
2026-09-07 18:52:37 -0700

    Refactor AbstractBaseClass to not use weird method reassignment.

--- src/dolt_annex/datatypes/pydantic.py
+++ src/dolt_annex/datatypes/pydantic.py
@@ -13,26 +13,31 @@ class StrictBaseModel(pydantic.BaseModel):
     model_config = ConfigDict(extra='forbid')
     
 class AbstractBaseModel(StrictBaseModel, ABC):
+    model_config = ConfigDict(extra='forbid', polymorphic_serialization=True)
 
-    # _type_map is a class variable that maps module names to the classes in that module that implement this abstract model.
-    _type_map: ClassVar[dict[str, dict[str, type[Self]]]]
+    # __type_map is a class variable that maps module names to the classes in that module that implement this abstract model.
+    __type_map: ClassVar[dict[str, dict[str, type[Self]]]]
+    __base_model: ClassVar[type[Self] | None] = None
+
+    def __new__(cls, **kwargs):
+        # Instantiating the base model returns the appropriate subclass based on the "type" field.
+        if cls is cls.__base_model:
+            if "type" not in kwargs:
+                raise ValueError(f"Missing 'type' field for {cls.__name__} instantiation.")
+            return cls.model_validate(kwargs)
+        return super().__new__(cls)
 
     def __init_subclass__(cls: type[Self], **kwargs):
-        cls._init()
+        # Direct subclasses of AbstractBaseModel are an abstract type and receive a type_map.
+        # Subclasses of those subclasses are implementations of that abstract type.
+        # They register themselves in their parent's type_map.
+        if cls.__base_model is None:
+            cls.__type_map = {}
+            cls.__base_model = cls
+        else:
+            module_name = cls.__module__.rsplit('.', 1)[-1]
+            cls.__base_model.__type_map.setdefault(module_name, dict())[cls.__name__] = cls
         return super().__init_subclass__(**kwargs)
-
-    @classmethod
-    def _init(cls: type[Self]) -> None:
-        cls._type_map = {}
-        @classmethod
-        def child_init_subclass__(child_cls: type[Self]):
-            module_name = child_cls.__module__.rsplit('.', 1)[-1]
-            cls._type_map.setdefault(module_name, dict())[child_cls.__name__] = child_cls
-        cls._init = child_init_subclass__
-
-    @classmethod
-    def is_abstract(cls: type[Self]) -> bool:
-        return bool(getattr(cls, "__abstractmethods__", False))
 
     @model_validator(mode='wrap')
     @classmethod
@@ -59,15 +64,15 @@ class AbstractBaseModel(StrictBaseModel, ABC):
         # because the subclass will register itself in __init_subclass__
         imported_module = importlib.import_module(f"..{module_name}", package=cls.__module__)
 
-        if module_name not in cls._type_map:
+        if module_name not in cls.__type_map:
             raise ImportError(f"module '{repr(imported_module)}' does not contain any classes that implement '{cls.__name__}'")
 
-        if len(cls._type_map[module_name]) == 1:
-            impl_cls = next(iter(cls._type_map[module_name].values()))
+        if len(cls.__type_map[module_name]) == 1:
+            impl_cls = next(iter(cls.__type_map[module_name].values()))
         elif class_name is None:
             raise ValueError(f"Multiple implementations of '{cls.__name__}' in '{module_name}'. Please specify the class name.")
         else:
-            impl_cls = cls._type_map[module_name][class_name]    
+            impl_cls = cls.__type_map[module_name][class_name]    
         
         return impl_cls(**v)
     
